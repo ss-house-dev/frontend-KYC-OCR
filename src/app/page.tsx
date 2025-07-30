1b48745fc90743bb89d54f0634c090c6a50e29f9
Irin <irin.m@kkumail.com>
2025-07-30 17:53:51 +0700

    refactor: simplify main component structure by removing unnecessary classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import IdCardOcrUploader from "@/components/IdCardOcrUploader";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-900">
+    <main >
       <IdCardOcrUploader />
     </main>
   );
