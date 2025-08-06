07394b737fa289953f9d9c4d4d83595b653ac0c2
pramin <owen63100@gmail.com>
2025-08-06 13:56:08 +0700

    fix: revise wording

--- src/app/card-id-consent/page.tsx
+++
@@ -1,5 +0,0 @@
-import VerifyIdContainer from "@/features/card-id-consent/containers/VerifyIdContainer";
-
-export default function VerifyPage() {
-  return <VerifyIdContainer />;
-}
---
+++ src/app/id-consent/page.tsx
@@ -0,0 +1,5 @@
+import VerifyIdContainer from "@/features/id-consent/containers/VerifyIdContainer";
+
+export default function VerifyPage() {
+  return <VerifyIdContainer />;
+}
