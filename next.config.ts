e544bf0ba291d1a7b55896c24d4a4d9434e292bc
Bibi <irin.m@kkumail.com>
2025-08-14 18:48:10 +0700

    fix: install webpack and proxy api

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,37 @@
-// next.config.ts (โค้ดนี้ถูกต้องแล้ว)
-
 import type { NextConfig } from "next";
 import { RuleSetRule } from "webpack";
+
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
       (rule: RuleSetRule): rule is RuleSetRule =>
-        typeof rule === 'object' && rule !== null && 'test' in rule && rule.test instanceof RegExp && rule.test.test(".svg")
+        typeof rule === "object" &&
+        rule !== null &&
+        "test" in rule &&
+        rule.test instanceof RegExp &&
+        rule.test.test(".svg")
     );
 
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      }
-    );
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
     if (fileLoaderRule) {
-        fileLoaderRule.exclude = /\.svg$/i;
+      fileLoaderRule.exclude = /\.svg$/i;
     }
-
     return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ocr/:path*",
+        destination: "http://kyra-kyc.ddns.net:3207/ocr/:path*",
+      },
+    ];
   },
 };
 
