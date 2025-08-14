610ddb5fb61abf64bc3e4dc09db6842b810d01ea
Bibi <irin.m@kkumail.com>
2025-08-14 18:29:46 +0700

    fix: fix id accept (owen)

---
+++ src/app/id-accept/page.tsx
@@ -0,0 +1,5 @@
+import VerifyIdContainer from "@/features/id-accept/containers/VerifyIdContainer";
+
+export default function VerifyPage() {
+return <VerifyIdContainer />;
+}
---
+++ src/features/id-accept/containers/VerifyIdContainer.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import VerifyIdView from "../components/VerifyIdView";
+
+export default function VerifyIdContainer() {
+  const [isChecked, setIsChecked] = useState(false);
+  const router = useRouter();
+
+  const handleCheckboxChange = (checked: boolean) => {
+    setIsChecked(checked);
+  };
+
+  const handleStartScan = () => {
+    if (!isChecked) return;
+    router.push("/scan-id-card"); 
+  };
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  return (
+    <VerifyIdView
+      isChecked={isChecked}
+      onCheckboxChange={handleCheckboxChange}
+      onStartScan={handleStartScan}
+      onBack={handleBack}
+    />
+  );
+}
