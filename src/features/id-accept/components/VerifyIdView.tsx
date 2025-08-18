29871b89110bb9b87ea26a2b6190f53f831e138d
pramin <owen63100@gmail.com>
2025-08-18 18:03:46 +0700

    fix:bug gradient and Background color in ios 15.xx

--- src/features/id-accept/components/VerifyIdView.tsx
+++ src/features/id-accept/components/VerifyIdView.tsx
@@ -267,7 +267,7 @@ export default function VerifyIdView({
           disabled={!isChecked}
           className={`w-full py-3 rounded-lg text-white font-bold text-base transition-colors duration-300 ${
             isChecked
-              ? "bg-gradient-to-b from-[#1F4293] to-[#246AEC] hover:from-[#246AEC] hover:to-[#1F4293]"
+              ? "bg-[#2152b6]"
               : "bg-gray-400 cursor-not-allowed"
           }`}
         >
