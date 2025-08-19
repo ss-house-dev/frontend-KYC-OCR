4f5469f296c14b567884e962e8260f1d9a57ada4
pramin <owen63100@gmail.com>
2025-08-19 14:16:04 +0700

    Link page in Back Arrow

--- src/app/preview-book-bank/page.tsx
+++ src/app/preview-book-bank/page.tsx
@@ -1,4 +1,5 @@
 import PreviewBookBankSection from "@/features/preview-book-bank/containers/PreviewBookBankSection";
+import Link from "next/link";
 
 const IconArrowLeft = () => (
   <svg
@@ -22,9 +23,16 @@ export default function Home() {
     <div className="bg-gray-50 flex justify-center">
       <div className="w-full max-w-md bg-white min-h-screen">
         <header className="relative flex items-center justify-center p-5 border-b border-gray-200">
-          <button className="absolute left-4 top-1/2 -translate-y-1/2">
+          <Link
+            href="/book-bank-accept"
+            aria-label="Back"
+            className="absolute left-4 top-1/2 -translate-y-1/2 p-2 rounded-lg
+             focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2 focus-visible:outline-[#2152b6]"
+          >
             <IconArrowLeft />
-          </button>
+            <span className="sr-only">Back</span>
+          </Link>
           <h1 className="text-xl font-bold text-[#0F2D73]">
             Book Bank Preview
           </h1>
