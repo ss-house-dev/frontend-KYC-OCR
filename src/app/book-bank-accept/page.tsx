8e9b5c16d665f2e0ea81a31fa7ee772deaab7fb4
pramin <owen63100@gmail.com>
2025-08-27 17:16:08 +0700

    test

--- src/app/book-bank-accept/page.tsx
+++ src/app/book-bank-accept/page.tsx
@@ -1,5 +1,8 @@
 import VerifyBookBankContainer from "@/features/book-bank-accept/containers/VerifyBookBankContainer";
-//
+/////////
+
+//tste
+////
 export default function VerifyPage() {
   return <VerifyBookBankContainer />;
 }
