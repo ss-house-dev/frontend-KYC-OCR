d8c2154828d89de74c4fd7a804b59971efce07e5
pramin <owen63100@gmail.com>
2025-09-02 16:52:57 +0700

    feat: Add user login page and components
    - Create logo SVG file for branding
    - Update Home redirect to user login page
    - Implement user login page with UserLoginContainer
    - Add BrandLogo component to display logo
    - Create UserLoginView component for email input and login functionality

---
+++ src/app/user-login/page.tsx
@@ -0,0 +1,5 @@
+import UserLoginContainer from "@/features/user-login/containers/UserLoginContainer";
+
+export default function Page() {
+  return <UserLoginContainer />;
+}
---
+++ src/features/user-login/components/BrandLogo.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+
+export default function BrandLogo() {
+  return (
+    <div className="mb-9 flex justify-center select-none">
+      <div className="relative h-16 w-[360px] sm:h-20 sm:w-[460px] flex items-center justify-center">
+        <Image
+          src="/user-login/logo-kyra-full.svg"
+          alt="Kyra"
+          width={242}
+          height={95}
+          className="object-contain "
+          priority
+        />
+      </div>
+    </div>
+  );
+}
