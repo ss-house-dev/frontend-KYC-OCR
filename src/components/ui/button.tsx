9f93f648412d96984323ca236b7e92fd066069e9
Bibi <irin.m@kkumail.com>
2025-09-05 14:03:25 +0700

    fix: change color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
         brand:
-          "bg-[#2152b6] text-white font-medium shadow-md hover:bg-[#1c46a0] rounded-sm",
+          "bg-[#1C55D9] text-white font-medium shadow-md hover:bg-[#1c46a0] rounded-sm",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
