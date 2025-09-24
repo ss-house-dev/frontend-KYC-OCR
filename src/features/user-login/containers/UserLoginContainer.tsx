e09b83957e7901704a59e9804ed98640567e966f
Bibi <irin.m@kkumail.com>
2025-09-24 12:29:31 +0700

    fix: change companyId

--- src/features/user-login/containers/UserLoginContainer.tsx
+++ src/features/user-login/containers/UserLoginContainer.tsx
@@ -48,7 +48,7 @@ export default function UserLoginContainer() {
       const res = await signIn("credentials", {
         email,
         //กำหนด ID ของบริษัท
-        companyId: "68bfb3b096a51a838c2bcd83",
+        companyId: "68d21d91cef5adbdd95fc076",
         callbackUrl,
         redirect: false,
       });
