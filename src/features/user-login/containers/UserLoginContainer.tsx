f846dac74bd88366ae1e8807471ce0b1fee725e0
Bibi <irin.m@kkumail.com>
2025-09-25 13:31:11 +0700

    fix: companyId and version docker compose

--- src/features/user-login/containers/UserLoginContainer.tsx
+++ src/features/user-login/containers/UserLoginContainer.tsx
@@ -48,7 +48,7 @@ export default function UserLoginContainer() {
       const res = await signIn("credentials", {
         email,
         //กำหนด ID ของบริษัท
-        companyId: "68bfb3b096a51a838c2bcd83",
+        companyId: process.env.NEXT_PUBLIC_COMPANY_ID,
         callbackUrl,
         redirect: false,
       });
