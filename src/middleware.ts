d397d2d93fb6bb823b2957ce362c7402e181de86
pramin <owen63100@gmail.com>
2025-10-03 15:34:42 +0700

    open middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,13 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    // "/id-accept",
-    // "/scan-id-card",
-    // "/preview-id-card",
-    // "/face-accept",
-    // "/scan-face",
-    // "/book-bank-accept",
-    // "/preview-book-bank",
-    // "/face-verification",
+    "/id-accept",
+    "/scan-id-card",
+    "/preview-id-card",
+    "/face-accept",
+    "/scan-face",
+    "/book-bank-accept",
+    "/preview-book-bank",
+    "/face-verification",
   ],
 };
